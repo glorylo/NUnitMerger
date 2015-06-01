24e4fe8b50dab48998b16470bf27fc27f2d54939
Leo Li <gqy117@gmail.com>
2015-06-01 15:54:52 +0800

    First version.

---
+++ RunNUnitTests/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunNUnitTests
+{
+    using System.IO;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = args[0];
+
+            var files = Directory.GetFiles(path);
+
+            NUnitMerger.MergeFiles(files, args[1]);
+        }
+    }
+}
